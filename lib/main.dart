d6aeaed2b6d24538bf0d7789f2b0eb05e2662d60
Dev Gala <114738592+devgala@users.noreply.github.com>
2023-03-22 02:18:34 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BBBMart"),
+
+      ),
+      body: Center(
+        child: const Text("Home Page"),
+      ),
+    );
+  }
+
+}
